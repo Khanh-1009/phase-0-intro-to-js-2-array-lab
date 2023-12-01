1f225baab7aca4c2f07283e67e69b019a66c3d3f
Khanh Tran <katetrannbkhanh@gmail.com>
2023-12-01 00:02:46 -0800

    commit tree 356628a26d85c95db4a9446e0a8db6c3582444a8

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    return cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    return cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(){
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    return cats.shift();
+}
+
+function appendCat(name){
+    let newCats = [];
+    return newCats = [...cats,name];
+}
+
+function prependCat(name){
+    let newKitty = [];
+    return newKitty = [name,...cats];
+}
+
+function removeLastCat(){
+    const newRemove = cats.slice(0,-1);
+    return newRemove;
+}
+
+function removeFirstCat(){
+    const newFirst = cats.slice(1);
+    return newFirst;
+}
